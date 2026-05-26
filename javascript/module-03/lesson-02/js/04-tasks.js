1dc68c111e66f09fd551425be763205d0f0862bd
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2026-05-26 18:55:37 +0300

    Prepare module 03

---
+++ javascript/module-03/lesson-02/js/04-tasks.js
@@ -0,0 +1,51 @@
+/*
+ * Напиши функцію logItems(items), яка отримує масив та використовує цикл,
+ * який для кожного елемента масиву буде виводити в консоль повідомлення у
+ * форматі <номер елемента> - <значення елемента>. Нумерація елементів
+ * повинна починатися з 1.
+ *
+ * Наприклад для першого елемента масиву ['Mango', 'Poly', 'Ajax'] з
+ * індексом 0 буде виведено 1 - Mango, а для індексу 2 виведе 3 - Ajax.
+ */
+
+function logItems(items) {}
+
+// logItems(["Mango", "Poly", "Ajax"]);
+// logItems(["🍎", "🍇", "🍑", "🍌", "🍋"]);
+
+/*
+ * Напиши функцію printInfo(names, phones) яка виводить
+ * у консоль ім'я та телефонний номер користувача. У параметри
+ * names та phones будуть передані рядки імен та телефонних номерів,
+ * розділені комами. Порядковий номер імен та телефонів у рядках
+ * вказують на відповідність. Кількість імен та телефонів
+ * гарантовано однакова.
+ */
+
+function printInfo(names, phones) {}
+
+printInfo(
+  "Jacob,William,Solomon,Artemis",
+  "89001234567,89001112233,890055566377,890055566300"
+);
+
+/*
+ * Напиши функцію formatTime(minutes) яка переведе значення
+ * minutes (кількість хвилин) у рядок у форматі годин
+ * та хвилин HH:MM.
+ */
+
+const hours = Math.floor(totalMinutes / 60);
+const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
+
+const doubleDigitHours = String(hours).padStart(2, 0);
+const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+
+function formatTime(minutes) {}
+
+// console.log(formatTime(70)); // "01:10"
+// console.log(formatTime(450)); // "07:30"
+// console.log(formatTime(1441)); // "24:01"
