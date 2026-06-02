02e1ec39af9e2182256f28b46978208910a9a2e5
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2026-06-02 20:06:31 +0300

    Prepare module 05

---
+++ javascript/module-05/lesson-01/js/03-foreach.js
@@ -0,0 +1,55 @@
+/**
+ * Метод forEach(callback)
+ * - Поелементо перебирає оригінальний масив
+ * - Нічого не повертає
+ * - Замінює класичний for, якщо не потрібно переривати цикл
+ */
+
+const numbers = [5, 10, 15, 20, 25];
+let total = 0;
+
+// console.log(total);
+
+/**
+ * Виконайте рефакторинг коду за допомогою методу forEach та стрілочні функції.
+ */
+function logItems(items) {
+  console.log(items);
+  for (let i = 0; i < items.length; i += 1) {
+    console.log(`${i + 1} - ${items[i]}`);
+  }
+}
+
+// logItems(["Mango", "Poly", "Ajax"]);
+// logItems(["🍎", "🍇", "🍑", "🍌", "🍋"]);
+
+/**
+ * Виконайте рефакторинг коду за допомогою методу forEach та стрілочні функції.
+ */
+function printContactsInfo({ names, phones }) {
+  const nameList = names.split(",");
+  const phoneList = phones.split(",");
+  for (let i = 0; i < nameList.length; i += 1) {
+    console.log(`${nameList[i]}: ${phoneList[i]}`);
+  }
+}
+
+// printContactsInfo({
+//   names: "Jacob,William,Solomon,Artemis",
+//   phones: "89001234567,89001112233,890055566377,890055566300",
+// });
+
+/**
+ * Виконайте рефакторинг коду за допомогою методу forEach та стрілочні функції.
+ */
+function calculateAverage(...args) {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total / args.length;
+}
+
+// console.log(calculateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calculateAverage(14, 8, 2)); // 8
+// console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
